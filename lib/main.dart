1afca077e8d5c95f1bd09e4ee272db84d03b409f
syafiq7974 <syafiqburhanuddin66@gmail.com>
2024-11-18 10:49:13 +0700

    Monitoring_Kehadiran Siswa

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/attendance_provider.dart';
-import 'screen/home_screen.dart';
+import 'models/home_screen.dart';
 
 void main() {
   runApp(
